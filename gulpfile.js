ca34a145489d3cbf84b6c1a618fb70ab75562a7b
ZhangDee <1325506619@qq.com>
2016-06-03 18:15:13 +0800

    更改输出路径

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require("gulp");
 var spritesmith = require("gulp.spritesmith");
 
+var path = {
+    icon : 'icon/*.png', // 图标路径
+    spritePath : 'sprites'   // 输出路径
+}
 /**
 * 雪碧图
 */
 gulp.task("sprite-css", function () {
-    var spriteData = gulp.src('icon/*.png')
+    var spriteData = gulp.src(path.icon)
         .pipe(spritesmith({
             imgName: 'sprite.png',
             cssName: 'sprite.css',
             padding: 5
         }));
-    return spriteData.pipe(gulp.dest('css'));
+    return spriteData.pipe(gulp.dest(path.spritePath));
 });
 
 gulp.task("sprite-scss", function () {
-    var spriteData = gulp.src('icon/*.png')
+    var spriteData = gulp.src(path.icon)
         .pipe(spritesmith({
             imgName: 'sprite.png',
             cssName: 'sprite.scss',
@@ -24,22 +28,32 @@ gulp.task("sprite-scss", function () {
             cssTemplate: 'scss.template.mustache',
             cssOpts: 'spriteSrc'
         }));
-    return spriteData.pipe(gulp.dest('scss/icon'));
+    return spriteData.pipe(gulp.dest(path.spritePath));
 });
 
 gulp.task("sprite-scss-default", function () {
-    var spriteData = gulp.src('icon/*.png')
+    var spriteData = gulp.src(path.icon)
         .pipe(spritesmith({
             imgName: 'sprite.png',
             cssName: 'sprite.scss',
             cssFormat: 'scss',
             padding: 5,
         }));
-    return spriteData.pipe(gulp.dest('scss/icon'));
+    return spriteData.pipe(gulp.dest(path.spritePath));
 });
+
+// 压缩图片
+// gulp.task("compress",function () {
+//     return gulp.src(path.spritePath)
+//         .pipe(imagemin({
+//             interlaced: true
+//         }))
+//         .pipe(gulp.dest(path.spritePath))
+// });
+
 
 // MAC 打开文件目录
 // gulp.task('sprite:open', function() {
 //   gulp.src('')
-//     .pipe(open({app: 'Finder', uri: configs.spritesOutputPath}));
+//     .pipe(open({app: 'Finder', uri: spritePath}));
 // })
